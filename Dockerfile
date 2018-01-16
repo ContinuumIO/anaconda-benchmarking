04613c54229df5adc21ee53a4820db1c953e9bb4
Michael Sarahan <msarahan@gmail.com>
2018-01-16 12:34:40 -0600

    fix up dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,14 +4,8 @@ MAINTAINER Michael Sarahan <msarahan@anaconda.com>
 # Set the locale
 ENV LANG en_US.UTF-8
 ENV LANGUAGE en_US:en
-ENV LC_ALL en_US.UTF-8
-
-RUN apt-get update && apt-get install -y perl python3 python3-pip python3-venv
-
-WORKDIR /build_scripts
-COPY install_miniconda.sh /build_scripts
-RUN ./install_miniconda.sh
+ENV LC_ALL C
 
 WORKDIR /envs
 COPY create_envs.sh /envs
-RUN ./create_envs.sh
+RUN bash create_envs.sh
--- create_envs.sh
+++ create_envs.sh
@@ -1,14 +1,25 @@
-python3 -m venv /envs/pip3
-source /envs/pip3/bin/activate
-pip install numpy==1.13.3 scipy scikit-learn toolz numexpr dask performance perf
-source deactivate
+#!/bin/bash
+set -ex
 
-/opt/conda/bin/conda create -y -p /envs/anaconda3 python=3.5 numpy=1.13
+mkdir -p /envs
+
+apt-get update
+apt-get install -y perl python3 python3-pip curl
+
+/usr/bin/pip3 install numpy==1.13.3 scipy performance perf
+
+fname="Miniconda3-latest-Linux-x86_64.sh"
+curl -LO https://repo.continuum.io/miniconda/$fname
+bash -x $fname -bfp /opt/conda
+/opt/conda/bin/conda clean -ptiy
+rm -rf Miniconda*
+
+/opt/conda/bin/conda create -y -p /envs/anaconda3 python=3.5 numpy=1.13 scipy
 source /envs/anaconda3/bin/activate
-pip install performance perf
+/envs/anaconda3/bin/pip install performance perf
 source /envs/anaconda3/bin/deactivate
-/opt/conda/bin/conda create -y -c intel -p /envs/intel3 python=3.5 numpy=1.13
+/opt/conda/bin/conda create -y -c intel -p /envs/intel3 python=3.5 numpy=1.13 scipy
 source /envs/intel3/bin/activate
-pip install performance perf
+/envs/intel3/bin/pip install performance perf
 source /envs/intel3/bin/deactivate
-conda clean -ay
+/opt/conda/bin/conda clean -ay
